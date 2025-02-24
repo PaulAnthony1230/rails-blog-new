b43d4cee7e46965728b86efdde2fb78ea904d498
PaulAnthony1230 <anthonypaulmabulay1230@gmail.com>
2025-02-24 13:16:01 +0800

    [RBN] Adding Devise

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true, length: { minimum: 10, maximum: 100 }
+  belongs_to :user
   # after_create :oncreate
 
   # def oncreate
